8adaad4b35a04b40748a3a92da99f9cc15bd3c32
Joseph Morris <joseph.morrisIII@hotmail.com>
2019-09-06 15:15:55 -0500

    Test

---
+++ hello_world.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear 
+echo "The script starts now."
+User="Joseph Morris"
+echo "Hi, $User!"
+echo
+
+echo "I will now fetch you a list of connected users:"
+echo 
+nslookup
+echo
+
+echo "I'm setting two variables now."
+Colour="Black"
+Value="9"
+echo "This is a string: $Colour"
+echo "And this is a number: $Value"
+echo
+
+echo "I'm giving you back your prompt now."
+echo
+~
+~
+~
